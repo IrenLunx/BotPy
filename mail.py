dac6321bfeab9648e44f66b6965c0b49c6e3ad79
Irina <ideryusheva@inbox.ru>
2022-08-09 22:42:12 +0700

    BotLes1

--- mail.py
+++ mail.py
@@ -7,18 +7,27 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
-async def answer_start_command(message: types.Message):
-    await message.answer(text='Привет, пупс!')
-
 @dp.message_handler(text=['Привет', 'Начать'])
+async def answer_start_command(message: types.Message):
+    await message.answer(text=f"Привет, {message['from']['first_name']}, что ты хочешь сделать?")
+    # print(message.from_user.first_name)
+
+@dp.message_handler(commands='add')
+async def answer_start_command(message: types.Message):
+    await message.answer(text='Позиция обавлена!')
+
+@dp.message_handler(commands='item')
+async def answer_start_command(message: types.Message):
+    await message.answer(text='У нас есть: редис, помидор и капуста')
+
+@dp.message_handler(commands='help')
+async def answer_start_command(message: types.Message):
+    await message.answer(text=f'/start - запуск программы\n/add - добавить позицию\n'
+                            f'/item - список товаров\n/help - команды')
+
+@dp.message_handler(text=['Редис', 'Помидор', 'Капуста'])
 async def answer_start_text(message: types.Message):
-    await message.answer(text='Опа!')
-    print(message)
-# Объединение функции
-# @dp.message_handler(commands='start')
-# @dp.message_handler(text=['Привет', 'Начать'])
-# async def answer_start_command(message: types.Message):
-#     await message.answer(text='Привет, пупс!')
+    await message.answer(text=f'У нас есть {message.text}!')
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
